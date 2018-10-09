0d18c3db15792996e588a7b046c964586b8bfe71
niesssiobhan <niesssiobhan@live.com>
2018-10-09 15:33:10 -0700

    Question one works

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-alert('It is alive!!');
+alert('Welcome! Let us play a guessing game so that you can get to know me better!');
+
+var userName = prompt('What is your name?');
+
+alert('Welcome ' + userName + '! How about we get started!');
+
+var answerOne = prompt('Do I have more than one child?').toLowerCase();
+
+if (answerOne === 'no' || answerOne === 'n'){
+    alert('You got it right! I only have one little girl!');
+    console.log('The user got Question One correct!');
+}else{
+    alert('Nope you are wrong, I only have one child');
+    console.log('The user got Question One wrong');
+}
